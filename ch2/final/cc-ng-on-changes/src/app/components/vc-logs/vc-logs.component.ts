b1b9b4ef431c5afb5fa1919061a1fa16c94493e6
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-08-31 22:18:05 +0200

    adding cc-ng-on-changes final files

---
+++ ch2/final/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-vc-logs',
+  templateUrl: './vc-logs.component.html',
+  styleUrls: ['./vc-logs.component.scss']
+})
+export class VcLogsComponent implements OnInit {
+  @Input() vName;
+  logs: string[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const currValue = changes.vName.currentValue;
+    if (changes.vName.isFirstChange()) {
+      this.logs.push(`initial version is ${currValue.trim()}`)
+    } else {
+      this.logs.push(`version changed to ${currValue.trim()}`)
+    }
+  }
+
+}
