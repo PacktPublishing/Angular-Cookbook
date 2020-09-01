c6160a0535b493a3981aa16c2c96438310cf62d4
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-09-01 20:30:08 +0200

    ch2: adding cc-view-child final files

---
+++ ch2/final/cc-view-child/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cc-view-child';
+  @ViewChild(GalleryComponent) gallery: GalleryComponent;
+
+  addNewPicture() {
+    this.gallery.pictures.unshift(this.gallery.generateImage());
+  }
+
+  removeFirstPicture() {
+    this.gallery.pictures.shift();
+  }
+}
