2cde8865af2dc365825315d326f1182d7f28f00d
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-09-01 19:14:23 +0200

    ch2: adding cc-template-vars final files

---
+++ ch2/final/cc-template-vars/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cc-template-vars';
+
+  addNewPicture(gallery: GalleryComponent) {
+    gallery.pictures.unshift(gallery.generateImage());
+  }
+
+  removeFirstPicture(gallery: GalleryComponent) {
+    gallery.pictures.shift();
+  }
+}
