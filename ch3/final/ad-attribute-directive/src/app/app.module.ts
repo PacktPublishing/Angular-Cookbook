05f5592eecd5b9eb75c16cd2db178563be1bdf51
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-09-08 22:37:02 +0200

    ch3: adding ad-attribute-directive final files

---
+++ ch3/final/ad-attribute-directive/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './directives/highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ch3/final/ad-attribute-directive/src/app/directives/highlight.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, Input, SimpleChanges, OnChanges, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnChanges {
+  @Input() highlightText = '';
+  @Input() highlightColor = 'yellow';
+  originalHTML = '';
+  constructor(private el: ElementRef) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.highlightText.firstChange) {
+      this.originalHTML = this.el.nativeElement.innerHTML;
+      return;
+    }
+    const { currentValue } = changes.highlightText;
+    if (currentValue) {
+      const regExp = new RegExp(`(${currentValue})`, 'gi')
+      this.el.nativeElement.innerHTML = this.originalHTML.replace(regExp, `<span style="background-color: ${this.highlightColor}">\$1</span>`)
+    } else {
+      this.el.nativeElement.innerHTML = this.originalHTML;
+    }
+  }
+
+}
