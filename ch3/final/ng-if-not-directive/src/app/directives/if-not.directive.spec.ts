5ad1c5e79933e935868caaef28f2f17fba10a272
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-09-16 19:55:48 +0200

    ch3: adding ng-if-not-directive final files

---
+++ ch3/final/ng-if-not-directive/src/app/directives/if-not.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IfNotDirective } from './if-not.directive';
+
+describe('IfNotDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IfNotDirective();
+    expect(directive).toBeTruthy();
+  });
+});
