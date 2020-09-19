7d114a52b2fd9867806ea91852f0b0ca67cbf3a0
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-09-19 16:18:03 +0200

    ch3: adding enhanced-template-type-checking final files

---
+++ ch3/final/enhanced-template-type-checking/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HighlightColor } from './directives/highlight.directive';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  searchText = '';
+  hColor: HighlightColor = HighlightColor.LightCoral;
+}
---
+++ ch3/final/enhanced-template-type-checking/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HighlightDirective } from '../app/directives/highlight.directive';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
