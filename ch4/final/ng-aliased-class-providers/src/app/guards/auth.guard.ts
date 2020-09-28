a1fe96375b9f3b45bcf41b61bc99a1bb045770f5
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-09-28 11:36:45 +0200

    ch4: adding ng-aliased-class-providers final files

---
+++ ch4/final/ng-aliased-class-providers/src/app/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor (private auth: AuthService, private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const loggedIn = !!this.auth.isLoggedIn();
+      if (!loggedIn) {
+        this.router.navigate(['/auth']);
+        return false;
+      }
+    return true;
+  }
+
+}
