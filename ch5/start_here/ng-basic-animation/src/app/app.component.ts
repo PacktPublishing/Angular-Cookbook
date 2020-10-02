0c727bdd0e20846cba3fe617c7ddece481cfef2b
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-10-02 22:40:47 +0200

    ch5: adding ng-basic-animation starter files

---
+++ ch5/start_here/ng-basic-animation/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SocialCardType } from './constants/social-card-type';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-dynamic-components';
+  selectedCardType: SocialCardType;
+  cardTypes = SocialCardType;
+
+  setCardType(type: SocialCardType) {
+    this.selectedCardType = type;
+  }
+}
