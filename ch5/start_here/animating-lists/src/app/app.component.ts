ceea37e6441dec81142bb450cbf8d2aee7551ad0
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-10-15 21:14:04 +0200

    ch5: adding animating-lists starter files

---
+++ ch5/start_here/animating-lists/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // DO NOT USE THE CODE BELOW IN PRODUCTION
+  // IT WILL CAUSE PERFORMANCE ISSUES
+  constructor(private auth: AuthService, private router: Router) {
+  }
+
+  get isLoggedIn() {
+    return this.auth.isLoggedIn();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/auth']);
+  }
+}
