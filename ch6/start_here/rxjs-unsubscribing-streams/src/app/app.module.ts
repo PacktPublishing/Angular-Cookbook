3c58bcca4528d2a1d856a2ed9d0a1740b6bc5816
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-11-05 18:27:50 +0100

    ch6: adding rxj-unsubscribing-streams starter files

---
+++ ch6/start_here/rxjs-unsubscribing-streams/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
