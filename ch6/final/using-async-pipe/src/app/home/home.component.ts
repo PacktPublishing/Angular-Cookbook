1ea0805ba73494a72c0b26169a45c42e633c484c
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-11-06 13:56:56 +0100

    ch6: adding using-async-pipe final files

---
+++ ch6/final/using-async-pipe/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { merge, Observable } from 'rxjs';
+import { interval } from 'rxjs/internal/observable/interval';
+import { map, takeWhile } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  inputStreamData = ['john wick', 'inception', 'interstellar'];
+  streamsOutput$: Observable<string[]>;
+  outputStreamData = []
+  constructor() { }
+
+  ngOnInit() {
+    this.startStream();
+  }
+
+  ngOnDestroy() {
+  }
+
+  startStream() {
+    const streamSource = interval(1500);
+    const secondStreamSource = interval(3000);
+    const fastestStreamSource = interval(500);
+    this.streamsOutput$ = merge(
+      streamSource,
+      secondStreamSource,
+      fastestStreamSource
+    ).pipe(
+      map(output => {
+        console.log(output)
+        this.outputStreamData = [...this.outputStreamData, output]
+        return this.outputStreamData;
+      })
+    )
+  }
+}
