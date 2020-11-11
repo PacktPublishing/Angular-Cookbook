d986cff9cbfb6c0146ad5822fdfff2249c96a1d5
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-11-11 21:24:20 +0100

    ch6: adding using-debouncetime-operator starter files

---
+++ ch6/start_here/using-debouncetime-operator/src/app/user-detail/user-detail.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../core/services/user.service';
+import { IUser } from '../core/interfaces/user.interface';
+import { ActivatedRoute } from '@angular/router';
+import { takeWhile, flatMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.scss']
+})
+export class UserDetailComponent implements OnInit, OnDestroy {
+  user: IUser;
+  similarUsers: IUser[];
+  isComponentAlive: boolean;
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.isComponentAlive = true;
+    this.route.paramMap.pipe(
+      takeWhile(() => !!this.isComponentAlive),
+      flatMap(params => {
+        this.user = null;
+        this.similarUsers = null;
+        const userId = params.get('uuid');
+        return this.userService.getUser(userId)
+          .pipe(
+            flatMap((user: IUser) => {
+              this.user = user;
+              return this.userService.getSimilarUsers(userId);
+            })
+          );
+      })
+    ).subscribe((similarUsers: IUser[]) => {
+      this.similarUsers = similarUsers;
+    })
+  }
+
+  ngOnDestroy() {
+    this.isComponentAlive = false;
+  }
+
+}
