81f8353832d96263ebf35cc2cec12e02474607ac
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-11-18 21:11:49 +0100

    ch7: adding ngrx-router-store final files

---
+++ ch7/final/ngrx-router-store/src/app/core/components/user-card/user-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '../../interfaces/user.interface';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.scss']
+})
+export class UserCardComponent implements OnInit {
+  @Input('user') user: IUser;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cardClicked() {
+    console.log(this.user)
+    this.router.navigate([`/user/${this.user.login.uuid}`]);
+  }
+
+}
