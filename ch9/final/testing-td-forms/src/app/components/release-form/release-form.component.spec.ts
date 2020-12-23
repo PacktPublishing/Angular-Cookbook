de6aefd70057afdb6600827b19476c7df7a1256a
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-12-23 02:29:19 +0100

    ch9: adding testing-td-forms final files

---
+++ ch9/final/testing-td-forms/src/app/components/release-form/release-form.component.spec.ts
@@ -0,0 +1,70 @@
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ReleaseLog } from 'src/app/classes/release-log';
+
+import { ReleaseFormComponent } from './release-form.component';
+
+describe('ReleaseFormComponent', () => {
+  let component: ReleaseFormComponent;
+  let fixture: ComponentFixture<ReleaseFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReleaseFormComponent ],
+      imports: [ FormsModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReleaseFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should submit a new release log with the correct input values', fakeAsync(async () => {
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    const CALENDAR_APP = component.apps[2];
+    spyOn(component.newReleaseLog, 'emit');
+
+    await fixture.whenStable(); // wait for Angular to configure the form
+
+    component.releaseForm.controls['version'].setValue('2.2.2');
+    component.releaseForm.controls['app'].setValue(CALENDAR_APP);
+    submitButton.click();
+    const expectedReleaseLog = new ReleaseLog(CALENDAR_APP, '2.2.2');
+    expect(component.newReleaseLog.emit).toHaveBeenCalledWith(expectedReleaseLog);
+  }));
+
+  it('should disable the submit button when version is incorrect', fakeAsync(async () => {
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    const CALENDAR_APP = component.apps[2];
+    spyOn(component.newReleaseLog, 'emit');
+
+    await fixture.whenStable(); // wait for Angular to configure the form
+    const expectedError = 'Invalid version provided. Please provide a valid version as (major.minor.patch)';
+    component.releaseForm.controls['version'].setValue('x.x.x');
+    component.releaseForm.controls['app'].setValue(CALENDAR_APP);
+    expect(() => component.formSubmit(component.releaseForm))
+      .toThrowError(expectedError);
+    fixture.detectChanges();
+    expect(submitButton.hasAttribute('disabled')).toBe(true);
+    expect(component.newReleaseLog.emit).not.toHaveBeenCalled();
+  }));
+
+  it('should disable the submit button when we don\'t have an app selected', fakeAsync(async () => {
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    spyOn(component.newReleaseLog, 'emit');
+
+    await fixture.whenStable(); // wait for Angular to configure the form
+    component.releaseForm.controls['version'].setValue('2.2.2');
+    component.releaseForm.controls['app'].setValue(null);
+    fixture.detectChanges();
+    expect(submitButton.hasAttribute('disabled')).toBe(true);
+    expect(component.newReleaseLog.emit).not.toHaveBeenCalled();
+  }));
+});
---
+++ ch9/final/testing-td-forms/src/app/components/release-form/release-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IReleaseLog, ReleaseLog } from 'src/app/classes/release-log';
+import { Apps } from 'src/app/constants/apps';
+import { REGEXES } from 'src/app/constants/regexes';
+
+@Component({
+  selector: 'app-release-form',
+  templateUrl: './release-form.component.html',
+  styleUrls: ['./release-form.component.scss']
+})
+export class ReleaseFormComponent implements OnInit {
+  @Output() newReleaseLog = new EventEmitter<ReleaseLog>();
+  @ViewChild('releaseForm') releaseForm: NgForm;
+  apps = Object.values(Apps);
+  versionInputRegex = REGEXES.SEMANTIC_VERSION;
+  newLog: IReleaseLog = {
+    app: Apps.CALENDAR,
+    version: '0.0.0'
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit(form: NgForm): void {
+    const { app, version } = form.value;
+    const newLog: ReleaseLog = new ReleaseLog(app, version)
+    this.newReleaseLog.emit(newLog);
+  }
+
+}
