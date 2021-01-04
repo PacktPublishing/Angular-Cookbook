06571c4523f155804d0f136c42e3f4a8c4c4cfde
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-01-04 20:05:48 +0500

    ch9: adding using-debounce-with-rfc app final files

---
+++ ch9/final/using-debounce-with-rfc/src/app/home/home.component.spec.ts
@@ -0,0 +1,43 @@
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent ],
+      imports: [ HttpClientModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should not send an http request before the debounceTime of 300ms', fakeAsync(async () => {
+    spyOn(component, 'searchUsers');
+    component.searchForm.get('username').setValue('iri');
+    tick(component.searchDebounceTime - 10); // less than desired debounce time
+    expect(component.searchUsers).not.toHaveBeenCalled();
+    discardPeriodicTasks();
+  }));
+
+  it('should send an http request after the debounceTime of 300ms', fakeAsync(async () => {
+    spyOn(component, 'searchUsers');
+    component.searchForm.get('username').setValue('iri');
+    tick(component.searchDebounceTime + 10); // more than desired debounce time
+    expect(component.searchUsers).toHaveBeenCalled();
+    discardPeriodicTasks();
+  }));
+});
---
+++ ch9/final/using-debounce-with-rfc/src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../core/services/user.service';
+import { IUser } from '../core/interfaces/user.interface';
+import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, takeWhile } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  users: IUser[];
+  searchForm: FormGroup
+  componentAlive: boolean;
+  searchDebounceTime = 300;
+  constructor(
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.componentAlive = true;
+    this.searchForm = new FormGroup({
+      username: new FormControl('', [])
+    })
+    this.searchUsers();
+    this.searchForm.get('username').valueChanges
+      .pipe(
+        debounceTime(this.searchDebounceTime),
+        takeWhile(() => !!this.componentAlive)
+      )
+      .subscribe(() => {
+        this.searchUsers();
+      })
+  }
+
+  searchUsers() {
+    const query = this.searchForm.get('username').value;
+    this.userService.searchUsers(query)
+      .subscribe(users => {
+        this.users = users;
+      });
+  }
+
+  ngOnDestroy() {}
+}
