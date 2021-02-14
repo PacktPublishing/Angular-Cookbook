28d54e5101ba1c1ba715dbe2dc716fcc17f8ef15
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-02-14 16:28:17 +0100

    ch10: using-list-key-manager starter files

---
+++ ch10/start_here/using-list-key-manager/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TheAmazingListComponent } from './components/the-amazing-list/the-amazing-list.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TheAmazingListComponent,
+    LoaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ScrollingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ch10/start_here/using-list-key-manager/src/interfaces/app-user-card.interface.ts
@@ -0,0 +1,3 @@
+export interface AppUserCard extends Faker.UserCard {
+  picture: string;
+}
