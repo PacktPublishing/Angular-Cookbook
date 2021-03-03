0fbb52993a3ebcc1a3b4f805ed1f624b36ebd638
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-03-03 16:30:10 +0100

    ch11: providing-global-mocks-for-jest final files

---
+++ ch11/final/providing-global-mocks-for-jest/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'Providing global mocks for Jest';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
