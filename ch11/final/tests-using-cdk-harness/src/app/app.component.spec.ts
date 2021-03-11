4b6c94cf5f0433b8a72bcb46c307e3c390757c1a
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-03-11 22:34:20 +0100

    ch11: tests-using-cdk-harness final files

---
+++ ch11/final/tests-using-cdk-harness/src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, async } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { ReleaseFormComponent } from './components/release-form/release-form.component';
+import { ReleaseLogsComponent } from './components/release-logs/release-logs.component';
+import { VersionControlComponent } from './components/version-control/version-control.component';
+
+const pageTitle = 'Writing tests using CDK Harnesses';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, ReactiveFormsModule],
+      declarations: [
+        AppComponent,
+        VersionControlComponent,
+        ReleaseFormComponent,
+        ReleaseLogsComponent,
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title '${pageTitle}'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual(pageTitle);
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.toolbar span').textContent).toContain(
+      pageTitle
+    );
+  });
+});
---
+++ ch11/final/tests-using-cdk-harness/src/app/components/version-control/version-control.component.spec.ts
@@ -0,0 +1,77 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Apps } from 'src/app/constants/apps';
+import { ReleaseFormComponent } from '../release-form/release-form.component';
+import { ReleaseLogsComponent } from '../release-logs/release-logs.component';
+
+import { VersionControlComponent } from './version-control.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ReleaseFormComponentHarness } from '../release-form/release-form.component.harness';
+import { ReleaseLogsComponentHarness } from '../release-logs/release-logs.component.harness';
+
+describe('VersionControlComponent', () => {
+  let component: VersionControlComponent;
+  let fixture: ComponentFixture<VersionControlComponent>;
+  let harnessLoader: HarnessLoader;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          VersionControlComponent,
+          ReleaseLogsComponent,
+          ReleaseFormComponent,
+        ],
+        imports: [ReactiveFormsModule],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VersionControlComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    harnessLoader = TestbedHarnessEnvironment.loader(fixture);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have the first app selected for the new release log', async () => {
+    const rfHarness = await harnessLoader.getHarness(
+      ReleaseFormComponentHarness
+    );
+    const appSelect = await rfHarness.getSelectedAppName();
+    expect(appSelect).toBe(Apps.DRIVE);
+  });
+
+  it('should show error on wrong version number input', async () => {
+    const rfHarness = await harnessLoader.getHarness(
+      ReleaseFormComponentHarness
+    );
+    await rfHarness.setNewAppVersion('abcd');
+    const isErrorshown = await rfHarness.isVersionErrorShown();
+    expect(isErrorshown).toBe(true);
+  });
+
+  it('should show the new log in the list after adding submitting a new log', async () => {
+    const rfHarness = await harnessLoader.getHarness(
+      ReleaseFormComponentHarness
+    );
+    const rLogsHarness = await harnessLoader.getHarness(
+      ReleaseLogsComponentHarness
+    );
+    let logsLength = await rLogsHarness.getLogsLength();
+    expect(logsLength).toBe(0); // no logs initially
+    const APP = Apps.DRIVE;
+    const VERSION = '2.3.6';
+    await rfHarness.setNewAppVersion(VERSION);
+    await rfHarness.clickSubmit();
+    logsLength = await rLogsHarness.getLogsLength();
+    expect(logsLength).toBe(1);
+    const isNewLogAdded = await rLogsHarness.validateLatestLog(VERSION, APP);
+    expect(isNewLogAdded).toBe(true);
+  });
+});
