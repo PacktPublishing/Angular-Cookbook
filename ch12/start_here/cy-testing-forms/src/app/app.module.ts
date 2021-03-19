def24184cbe623f48ca2e7c3a1a4d5314ad81913
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-03-19 21:37:15 +0100

    ch12: cy-testing-forms starter files

---
+++ ch12/start_here/cy-testing-forms/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
