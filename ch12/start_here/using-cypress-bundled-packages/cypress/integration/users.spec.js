0d251e1a5227e2f5415601dcdf73ae79d3e22835
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-03-27 19:28:45 +0100

    ch12: using-cypress-bundled-packages app starter files

---
+++ ch12/start_here/using-cypress-bundled-packages/cypress/integration/users.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context('Users', () => {
+  beforeEach(() => {
+    cy.visit('/users');
+  });
+
+  it('should get the users list from the server and display', () => {
+    cy.get('app-user-card').should((domList) => {
+      expect(domList.length).equal(10);
+    });
+  });
+
+  it('should get the users list on searching', () => {
+    cy.intercept('https://api.randomuser.me/*').as('searchUsers');
+    cy.get('#searchInput').type('irin');
+    cy.wait('@searchUsers');
+    cy.get('app-user-card').should((domList) => {
+      expect(domList.length).equal(1);
+    });
+  });
+});
---
+++ ch12/start_here/using-cypress-bundled-packages/src/app/users/users.component.spec.ts
@@ -0,0 +1,68 @@
+import { HttpClientModule } from '@angular/common/http';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  DUMMY_USERS,
+  UserServiceMock,
+} from 'src/__mocks__/services/user.service.mock';
+import { UserCardComponent } from '../core/components/user-card/user-card.component';
+import { UserService } from '../core/services/user.service';
+
+import { UsersComponent } from './users.component';
+
+describe('UsersComponent', () => {
+  let component: UsersComponent;
+  let fixture: ComponentFixture<UsersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UsersComponent, UserCardComponent],
+      providers: [
+        {
+          provide: UserService,
+          useClass: UserServiceMock,
+        },
+      ],
+      imports: [HttpClientModule, ReactiveFormsModule, RouterTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call searchUsers() method on component init', () => {
+    spyOn(component, 'searchUsers');
+    component.ngOnInit();
+    expect(component.searchUsers).toHaveBeenCalled();
+  });
+
+  it('should get users back from the API component init', fakeAsync(() => {
+    component.ngOnInit();
+    tick(500);
+    expect(component.users.length).toBe(2);
+    expect(component.users).toEqual(DUMMY_USERS);
+  }));
+
+  it('should get the searched users from the API upon searching', fakeAsync(() => {
+    component.searchForm.get('username').setValue('hall');
+    // the second record in our DUMMY_USERS array has the name Mrs Indie Hall
+    const expectedUsersList = [DUMMY_USERS[1]];
+    component.searchUsers();
+    tick(500);
+    expect(component.users.length).toBe(1);
+    expect(component.users).toEqual(expectedUsersList);
+  }));
+});
