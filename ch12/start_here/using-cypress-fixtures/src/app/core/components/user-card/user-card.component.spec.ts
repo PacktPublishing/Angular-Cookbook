6b016c2ac5699d3eb5d1dcdadbd9dfb0ef700a59
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-03-28 17:08:45 +0200

    ch12: using-cypress-fixtures starter files

---
+++ ch12/start_here/using-cypress-fixtures/src/app/core/components/user-card/user-card.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { UserCardComponent } from './user-card.component';
+
+describe('UserCardComponent', () => {
+  let component: UserCardComponent;
+  let fixture: ComponentFixture<UserCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserCardComponent ],
+      imports: [RouterTestingModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
