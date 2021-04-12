ddbce13ab62666db10c403c6b6525c43b14cd552
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-04-12 21:41:26 +0200

    ch13: using-webpack-bundle-analyzer final files

---
+++ ch13/final/using-webpack-bundle-analyzer/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ch13/final/using-webpack-bundle-analyzer/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'login'
+}, {
+  path: 'login',
+  component: LoginComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
