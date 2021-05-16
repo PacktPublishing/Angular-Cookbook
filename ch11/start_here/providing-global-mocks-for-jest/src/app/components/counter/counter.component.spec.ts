69d93476f6c33626364b1eeac555bfb43a1b028f
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-05-16 03:17:01 +0200

    update providing-global-mocks-for-jest

--- ch11/start_here/providing-global-mocks-for-jest/src/app/components/counter/counter.component.spec.ts
+++ ch11/start_here/providing-global-mocks-for-jest/src/app/components/counter/counter.component.spec.ts
@@ -37,4 +37,39 @@ describe('CounterComponent', () => {
     component.reset();
     expect(component.counter).toBe(0);
   });
+  it('should show an alert when the counter value goes above the MAX_VALUE', () => {
+    spyOn(window, 'alert');
+    component.counter = component.MAX_VALUE;
+    component.increment();
+    expect(window.alert).toHaveBeenCalledWith('Value too high');
+    expect(component.counter).toBe(component.MAX_VALUE);
+  });
+  it('should show an alert when the counter value goes above the MAX_VALUE', () => {
+    spyOn(window, 'alert');
+    component.counter = component.MIN_VALUE;
+    component.decrement();
+    expect(window.alert).toHaveBeenCalledWith('Value too low');
+    expect(component.counter).toBe(component.MIN_VALUE);
+  });
+  it('should call the localStorage.getItem method on component init', () => {
+    spyOn(localStorage, 'getItem');
+    component.ngOnInit();
+    expect(localStorage.getItem).toBeCalled();
+  });
+  it('should retrieve the last saved value from localStorage on component init', () => {
+    localStorage.setItem('counterValue', '12');
+    component.ngOnInit();
+    expect(component.counter).toBe(12);
+  });
+  it('should save the new counterValue to localStorage on increment, decrement and reset', () => {
+    spyOn(localStorage, 'setItem');
+    component.counter = 0;
+    component.increment();
+    expect(localStorage.setItem).toHaveBeenCalledWith('counterValue', '1');
+    component.counter = 20;
+    component.decrement();
+    expect(localStorage.setItem).toHaveBeenCalledWith('counterValue', '19');
+    component.reset();
+    expect(localStorage.setItem).toHaveBeenCalledWith('counterValue', '0');
+  });
 });
