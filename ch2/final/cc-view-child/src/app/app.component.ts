c1a7c1e5c43b71730f5cd8770f06f44c63138dc8
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-05-30 23:21:50 +0200

    updating cc-template-vars

--- ch2/final/cc-view-child/src/app/app.component.ts
+++ ch2/final/cc-view-child/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 import { GalleryComponent } from './components/gallery/gallery.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cc-view-child';
-  @ViewChild(GalleryComponent) gallery: GalleryComponent;
+  @ViewChild(GalleryComponent) gallery;
 
   addNewPicture() {
     this.gallery.pictures.unshift(this.gallery.generateImage());
--- ch2/start_here/ng-dynamic-components/src/app/components/twitter-card/twitter-card.component.spec.ts
+++ ch2/start_here/ng-dynamic-components/src/app/components/twitter-card/twitter-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TwitterCardComponent } from './twitter-card.component';
 
@@ -6,7 +6,7 @@ describe('TwitterCardComponent', () => {
   let component: TwitterCardComponent;
   let fixture: ComponentFixture<TwitterCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TwitterCardComponent ]
     })
