7480ab14635718217f6c4c9890b1ce8d26a557d6
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-06-09 03:16:05 +0200

    renamed chapter 9 to 8

--- ch8/final/route-preloading-strategies/src/app/interfaces/bucket-service.ts
+++
@@ -1,7 +0,0 @@
-
-import { IFruit } from './fruit.interface';
-export interface IBucketService {
-  loadItems(): void
-  addItem(fruit: IFruit) : void
-  removeItem(fruit: IFruit): void
-}
--- ch8/final/routing-global-loader/src/app/interfaces/fruit.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Fruit } from "../constants/fruit";
-
-export interface IFruit {
-  id: number;
-  name: Fruit;
-}
