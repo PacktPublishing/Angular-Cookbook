adef0f9caa5aca2660a3bc4c367408c75c9e152e
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-09-01 10:11:04 +0200

    fix(using-switchmap-opeartor): remove solution (#31)

--- chapter05/start_here/using-switchmap-operator/src/app/home/home.component.ts
+++ chapter05/start_here/using-switchmap-operator/src/app/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { UserService } from '../core/services/user.service';
 import { IUser } from '../core/interfaces/user.interface';
 import { FormControl, FormGroup } from '@angular/forms';
-import { switchMap, takeWhile } from 'rxjs/operators';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -26,10 +25,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       .get('username')
       .valueChanges.pipe(
         takeWhile(() => !!this.componentAlive),
-        switchMap((query) => this.userService.searchUsers(query))
       )
-      .subscribe((users) => {
-        this.users = users;
+      .subscribe(() => {
+        this.searchUsers();
       });
   }
 
